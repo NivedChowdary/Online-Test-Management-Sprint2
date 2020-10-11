bf4eff9252eb8350e0509d72ac700f48f99e50c3
Nived <Nived@LAPTOP-BF24ID9Q>
2020-10-11 11:23:17 +0530

    Comments added in test-ms

--- otms-test-ms/src/main/java/com/capg/otms/test/controller/TestController.java
+++ otms-test-ms/src/main/java/com/capg/otms/test/controller/TestController.java
@@ -44,44 +44,142 @@ public class TestController {
 	TestBean testBean=new TestBean( "spring Test", LocalTime.of(1, 30), questions, 100, 0, 1L, LocalDateTime.of(2020, 05,2, 14, 0), LocalDateTime.of(2020, 05,2, 15, 30));
 	service.addtest(testBean);
 	}
-	
+	/******************************************
+	  - Method Name         : getTest
+	  - Input Parameters    : long testId
+	  - Return Type         : Test
+	  - End Point Url       : /id
+	  - Request Method Type : GetMapping 
+	  - Author              : Nived 
+	  - Creation Date       : 28/09/2020
+	  - Description         : Getting the Test detail from the database
+	 ******************************************/
 	@GetMapping("/id/{testId}")
 	public ResponseEntity<TestBean> getTest(@PathVariable long testId){
 	return service.getTest(testId);	
 	}
+	/******************************************
+	  - Method Name         : getAllTest
+	  - Input Parameters    : none
+	  - Return Type         : Test
+	  - End Point Url       : /all
+	  - Request Method Type : GetMapping 
+	  - Author              : Nived 
+	  - Creation Date       : 28/09/2020
+	  - Description         : Getting the Test details from the database
+	 ******************************************/
 	@GetMapping("/all")
 	public ResponseEntity<List<TestBean>> getAllTests(){
 	return service.fetchAllTests();	
 	}
+	/******************************************
+	  - Method Name         : addTest
+	  - Input Parameters    : Test testBean
+	  - Return Type         : Test
+	  - End Point Url       : /add
+	  - Request Method Type : PostMapping 
+	  - Author              : Nived 
+	  - Creation Date       : 28/09/2020
+	  - Description         : Adding the Test details in to the database
+	 ******************************************/
 	@PostMapping("/add")
 	public ResponseEntity<TestBean> addTest(@RequestBody TestBean testBean){
 		return service.addtest(testBean);
 	}
+	/******************************************
+	  - Method Name         : updateTest
+	  - Input Parameters    : Test newTestData, long testId
+	  - Return Type         : Test
+	  - End Point Url       : /update
+	  - Request Method Type : PutMapping 
+	  - Author              : Nived 
+	  - Creation Date       : 28/09/2020
+	  - Description         : Updating the Test details in to the database
+	 ******************************************/
 	@PutMapping("/update/{testId}")
 	public ResponseEntity<TestBean> updateTest(@RequestBody TestBean testBean,@PathVariable long testId){
 		return service.updateTest(testBean ,testId);
 	}	
+	/******************************************
+	  - Method Name         : assignQuestion
+	  - Input Parameters    : long testId, long QuestionId
+	  - Return Type         : Test
+	  - End Point Url       : /assign
+	  - Request Method Type : PutMapping 
+	  - Author              : Nived 
+	  - Creation Date       : 28/09/2020
+	  - Description         : assigning test to user
+	 ******************************************/
 	@PutMapping("/assign/{testId}/question/{questionId}")
 	public ResponseEntity<TestBean> assignQuestion(@PathVariable long testId, @PathVariable long questionId) {
 		return service.assignQuestion(testId, questionId);
 	}
-	
+	/******************************************
+	  - Method Name         : deleteTest
+	  - Input Parameters    : long testId
+	  - Return Type         : Test
+	  - End Point Url       : /delete
+	  - Request Method Type : DeleteMapping 
+	  - Author              : Nived 
+	  - Creation Date       : 28/09/2020
+	  - Description         : deleting the Test details from the database
+	 ******************************************/
 	@DeleteMapping("delete/id/{testId}")
 	public ResponseEntity<TestBean> deleteTest(@PathVariable long testId){
 	return service.deleteTest(testId);
 	}
+	/******************************************
+	  - Method Name         : calculateTotalMarks
+	  - Input Parameters    : long testId
+	  - Return Type         : Double
+	  - End Point Url       : /calculate
+	  - Request Method Type : GetMapping 
+	  - Author              : Nived 
+	  - Creation Date       : 28/09/2020
+	  - Description         : Calculating total marks of user
+	 ******************************************/
 	@GetMapping("/calculate/{testId}")
 	public ResponseEntity<Double> calculateTotalMarks(@PathVariable long testId){
 		return service.calculateTotalMarks(testId);
 	}
+	/******************************************
+	  - Method Name         : fetchQuestion
+	  - Input Parameters    : long testId
+	  - Return Type         : Question
+	  - End Point Url       : /question
+	  - Request Method Type : PutMapping 
+	  - Author              : Nived 
+	  - Creation Date       : 28/09/2020
+	  - Description         : fetching questions
+	 ******************************************/
 	@GetMapping("/question/{questionId}")
 	public ResponseEntity<Question> fetchQuestion(@PathVariable long questionId){
 		return service.fetchQuestion(questionId);
 	}
+	/******************************************
+	  - Method Name         : setTestQuestions
+	  - Input Parameters    : long testId, long questionIds
+	  - Return Type         : Test
+	  - End Point Url       : /setTestQuestions
+	  - Request Method Type : PutMapping 
+	  - Author              : Nived 
+	  - Creation Date       : 28/09/2020
+	  - Description         : assigning questions to test
+	 ******************************************/
 	@PutMapping("/setTestQuestions/testId/{testId}")
-	public ResponseEntity<TestBean> setTestQuestions(@PathVariable long testId, @RequestBody Set<Long> qIds){
-		return service.setTestQuestions(testId, qIds);
+	public ResponseEntity<TestBean> setTestQuestions(@PathVariable long testId, @RequestBody Set<Long> questionIds){
+		return service.setTestQuestions(testId, questionIds);
 	}
+	/******************************************
+	  - Method Name         : getTestQuestions
+	  - Input Parameters    : long testId
+	  - Return Type         : Question
+	  - End Point Url       : /setTestQuestions
+	  - Request Method Type : PutMapping 
+	  - Author              : Nived 
+	  - Creation Date       : 28/09/2020
+	  - Description         : assigning questions to test
+	 ******************************************/
 	@GetMapping("/questions/{testId}")
 	public ResponseEntity<List<Question>> getTestQuestions(@PathVariable long testId){
 		return service.getTestQuestions(testId);
